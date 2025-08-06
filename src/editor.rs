276659d1e8023f87e086324bf994e3e3170e95ab
prolletto64 <github@prolletto64.eu>
2025-08-06 18:46:17 +0200

    formatting

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,12 @@
-use crossterm::terminal::{enable_raw_mode,disable_raw_mode,Clear,ClearType};
-use crossterm::event::{read, Event::Key ,KeyModifiers, KeyCode::{Char, Enter, Backspace}};
+use crossterm::cursor::{DisableBlinking, EnableBlinking, MoveTo};
+use crossterm::event::{
+    read,
+    Event::Key,
+    KeyCode::{Backspace, Char, Enter},
+    KeyModifiers,
+};
 use crossterm::execute;
-use crossterm::cursor::{MoveTo, EnableBlinking, DisableBlinking};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use std::io::{stdout, Write};
 use std::option::Option::Some;
 
@@ -10,79 +15,79 @@ pub struct Editor {
 }
 
 impl Editor {
-    pub fn default() -> Self{
-        Editor{
+    pub fn default() -> Self {
+        Editor {
             buffer: String::from(""),
         }
     }
-    pub fn run(&mut self){
-       Editor::init();
-       Editor::main_loop(&mut self.buffer);
-       Editor::close();
+    pub fn run(&mut self) {
+        Editor::init();
+        Editor::main_loop(&mut self.buffer);
+        Editor::close();
     }
-    fn init(){
+    fn init() {
         let _ = enable_raw_mode();
         Editor::clear_screen();
-        match execute!(stdout(),EnableBlinking){
+        match execute!(stdout(), EnableBlinking) {
             Ok(_) => (),
             Err(err) => panic!("{err}"),
         }
     }
-    fn close(){
+    fn close() {
         let _ = disable_raw_mode();
         Editor::clear_screen();
-        match execute!(stdout(),DisableBlinking){
+        match execute!(stdout(), DisableBlinking) {
             Ok(_) => (),
             Err(err) => panic!("{err}"),
         }
     }
-    fn clear_screen(){
-        match execute!(stdout(),Clear(ClearType::All)){
+    fn clear_screen() {
+        match execute!(stdout(), Clear(ClearType::All)) {
             Ok(_) => (),
             Err(err) => panic!("{err}"),
         }
-        match execute!(stdout(),MoveTo(0,0)){
+        match execute!(stdout(), MoveTo(0, 0)) {
             Ok(_) => (),
             Err(err) => panic!("{err}"),
         }
     }
-    fn main_loop(buffer: &mut String){
-        loop{
-            match read(){
+    fn main_loop(buffer: &mut String) {
+        loop {
+            match read() {
                 Ok(Key(event)) => {
-                    if event.modifiers.contains(KeyModifiers::CONTROL){
-                        match event.code{
+                    if event.modifiers.contains(KeyModifiers::CONTROL) {
+                        match event.code {
                             Char('q' | 'c') => {
                                 break;
-                            },
+                            }
                             Char(c) => {
                                 buffer.push(c);
-                            },
+                            }
                             _ => (),
                         }
-                    }else{
-                        match event.code{
-                            Char(c)=>{
+                    } else {
+                        match event.code {
+                            Char(c) => {
                                 buffer.push(c);
-                            },
+                            }
                             Enter => {
                                 buffer.push_str("\r\n");
-                            },
+                            }
                             Backspace => {
                                 buffer.pop();
-                                if buffer.chars().last() == Some('\r'){
+                                if buffer.chars().last() == Some('\r') {
                                     buffer.pop();
                                 }
-                            },
+                            }
                             _ => (),
                         }
                     }
-                },
+                }
                 Err(err) => println!("Error: {err}"),
                 _ => (),
             }
             Editor::clear_screen();
-            print!("{}",buffer);
+            print!("{}", buffer);
             stdout().flush().unwrap();
         }
     }
